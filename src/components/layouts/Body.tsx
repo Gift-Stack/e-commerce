e54e8d64f2a7aa02df2c8e7d7cf174f50885ce87
Gift <gkenny896@gmail.com>
2021-03-24 07:33:07 +0100

    first media query set

--- src/components/layouts/Body.tsx
+++ src/components/layouts/Body.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import { Text, Grid, Box } from '@chakra-ui/react';
+import { Text, Grid, Box, useMediaQuery } from '@chakra-ui/react';
 import products from '../../products.json';
 import { Products } from './Products';
 
 export const Body = () => {
+  const [isSmallerThan700] = useMediaQuery('(max-width: 700px)');
   return (
-    <Box>
+    <Box w='100%'>
       <Text fontSize='4xl' fontWeight='bold'>
         Our Products
       </Text>
       <Box p='4'>
-        <Grid templateColumns='repeat(3,1fr)' gap={6}>
+        <Grid
+          templateColumns={`repeat(${isSmallerThan700 ? 2 : 3},1fr)`}
+          gap={6}
+        >
           {products.map((product) => (
             <Products product={product} />
           ))}
--- src/components/routes/Cart.tsx
+++ src/components/routes/Cart.tsx
@@ -2,14 +2,6 @@ import React, { useEffect, useState } from 'react';
 
 const Cart = () => {
   const [cart, setCart] = useState<object>([]);
-
-  useEffect(() => {
-    some();
-  }, [cart]);
-  const some = () => {
-    console.log(cart);
-    console.log(setCart({ name: 'gift' }));
-  };
-  return <div>Cart</div>;
+  return <div>{cart ? 'Cart' : { cart }}</div>;
 };
 export default Cart;
