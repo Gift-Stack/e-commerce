73c37cd603c5227871d64a7976d7af5ed6df4944
Gift <gkenny896@gmail.com>
2021-04-01 15:50:09 +0100

    logo changed

--- src/components/routes/Cart.tsx
+++ src/components/routes/Cart.tsx
@@ -10,9 +10,11 @@ const Cart = () => {
   const cart = productContext.cart;
   return (
     <Box>
-      <Text fontSize='4xl' textAlign='left' p={5} fontWeight='bold'>
-        Shopping Cart
-      </Text>
+      {cart.length !== 0 && (
+        <Text fontSize='4xl' textAlign='left' p={5} fontWeight='bold'>
+          Shopping Cart
+        </Text>
+      )}
       {cart.length === 0 && (
         <React.Fragment>
           <Center fontSize='3xl' fontStyle='italic' color='GrayText'>
